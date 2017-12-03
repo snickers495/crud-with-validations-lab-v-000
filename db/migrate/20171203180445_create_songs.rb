57a25605788c02de4d5d3f41ffb8e597fb4c49d0
Lisa Lee <snickers495@gmail.com>
2017-12-03 19:24:17 +0000

    automatically backed up by learn

--- db/migrate/20171203180445_create_songs.rb
+++ db/migrate/20171203180445_create_songs.rb
@@ -2,7 +2,7 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :title
-      t.boolean :release
+      t.boolean :released
       t.integer :release_year
       t.string :artist_name
       t.string :genre
